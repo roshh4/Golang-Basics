81c71130eabaf195de1d3116a09fa5a7f0404b0b
roshh4 <roshinidharmaraj@gmail.com>
2025-06-18 13:04:11 -0300

    maps

--- array.go
+++ array.go
@@ -5,8 +5,19 @@ import (
 	"sort"
 )
 
+func mapp(){
+	varia := map[int]string{
+		1 : "rosh",
+		2 : "gayboi",
+		3 : "hii",
+	}
+	name := varia[1]
+	fmt.Println(name)
+}
+
 func main(){
-	fmt.Print("hiii")
+	fmt.Print("hiii\n")
 	new := []int {1,7,8,2,4};
 	sort.Ints(new)
+	mapp()
 }
