aca5b58cdc7386d62b7c7b7b6234722bcbf41ecf
roshh4 <roshinidharmaraj@gmail.com>
2025-06-18 11:39:22 -0300

    variable declaration

---
+++ variable.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var dessert string = "cookie"
+	fmt.Println("i love", dessert)
+	var age int = 20;
+	name := "rosh"
+	fmt.Println("Im",name,"and im",age, "years old.")
+}
