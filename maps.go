1ac390b44a8c662a89cd94e64e0fcb30131bf8a1
roshh4 <roshinidharmaraj@gmail.com>
2025-06-20 13:49:01 +0530

    maps

---
+++ maps.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	info := map[string]int{
+		"rosh": 20,
+		"avinash": 21,
+		"dorito": 2,
+		"bhuvaan": 19,
+	}
+
+	for key := range info {
+		fmt.Println(key, ":", info[key])
+	}
+
+	val, ok := info["dorito"]
+	if ok {
+		fmt.Println("Dorito is",val, "years old")
+	} else {
+		fmt.Println("dorito is missing :(")
+	}
+}
